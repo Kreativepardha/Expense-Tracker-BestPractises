690b85250a61529f7a6b3b1458e5fb7d5d525250
Kreativepardha <saradhipardha12@gmail.com>
2025-02-04 11:51:38 +0530

    .

--- be/src/controllers/authController.ts
+++ be/src/controllers/authController.ts
@@ -1,9 +1,51 @@
 import { Request, Response } from 'express'
-import { registerSchema } from '../validations/authValidator'
-import { registerService } from '../services/authService'
+import { loginSchema, registerSchema } from '../validations/authValidator'
+import { loginService, logoutService, registerService } from '../services/authService'
+import logger from '../utils/logger'
+
+
+
 
 export const register = async (req: Request, res: Response) => {
+    try {
     const validatedData = registerSchema.parse(req.body)
+
     const response = await registerService(validatedData)
     res.status(201).json(response)
+} catch (er: any) {
+    logger.error(er.message)
+        res.status(400).json({
+            message: er.message || 'regitsration failed'
+        })
+}
+}
+
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const validatedData = loginSchema.parse(req.body)
+        const res = await loginService(validatedData)
+        res.cookie('refreshToken', res.refreshToken, {
+            httpOnly: true,
+            secure: //,
+        });
+        res.json(res)
+    } catch (er) {
+        res.status(400).json({
+            message: er.message
+        })
+    }
+}
+
+
+export const logout = async (req: Request, res: Response) => {
+    try {
+        await logoutService(req.cookies.refreshToken)
+        res.clearCookie('refreshToken')
+        res.json({
+            message: "Logged out succesfully"
+        })
+    } catch (er) {
+        res.status(500).json({ message: "Server error" });
+    }
 }
--- be/src/services/authService.ts
+++ be/src/services/authService.ts
@@ -1,20 +1,49 @@
 import { config } from '../config/config'
 import User from '../models/userModel'
+import { RegisterData } from '../types/authTypes'
 import { CustomError } from '../utils/errorHandler'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
-export const registerService = async (Data: any) => {
+const generateToken = (userId: string) => {
+    const accessToken = jwt.sign({ userId }, config.JWT_SECRET, { expiresIn: '1h'})
+    return accessToken;
+}
+
+
+export const registerService = async (Data: RegisterData) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const { firstName, lastName, email, password } = Data
     const existingUser = await User.findOne({ email })
 
     if (existingUser) throw new CustomError('User Already exists', 400)
 
-    const user = new User({ firstName, lastName, email, password })
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    const hashedPass = bcrypt.hashSync(password, 10)
+    
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const user = new User({ firstName, lastName, email, password: hashedPass })
     await user.save()
 
-    const token = jwt.sign({ userId: user._id }, config.JWT_SECRET as string, {
-        expiresIn: config.JWT_EXPIRATION
-    })
+    const token = generateToken(user._id)
 
-    return { message: 'User registered successfully', token }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return { message: 'User registered successfully', ...token }
+}
+
+export const loginService = async({ email, password}: any) => {
+    const existingUser = await User.findOne({ email })
+    if(!existingUser) throw new CustomError('Invalid Credentials', 400)
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+        const isMatch = bcrypt.compareSync(password, existingUser.password)
+        if(!isMatch) throw new CustomError('invalid credentials', 400)
+
+            return generateToken(existingUser._id)
+}
+
+export const logoutService = async (token: string) => {
+    return {
+        message: 'User LOgged Out'
+    }
 }
